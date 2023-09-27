62add3b248ba92d58373106205060b6164e79c3d
dreyanfranco <dreyanfranco@gmail.com>
2023-09-27 16:04:49 +0200

    contact form

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -1,8 +1,44 @@
+'use client'
+
+import { motion } from 'framer-motion'
 import React from 'react'
+import { FaPaperPlane } from 'react-icons/fa'
+import SectionHeading from './section-heading'
 
 const Contact = () => {
     return (
-        <section id='contact'>Contact me</section>
+        <motion.section
+            id='contact'
+            className='mb-20 sm:mb-28 w-[min(100%,38rem)] text-center'
+            initial={{
+                opacity: 0
+            }}
+            whileInView={{
+                opacity: 1
+            }}
+            transition={{
+                duration: 1
+            }}
+        >
+            <SectionHeading>Contact me</SectionHeading>
+            <p className='text-gray-700 -mt-6'>Please contact me at{' '}
+                <a className='underline' href="mailto:dreyanfranco@gmail.com">
+                    dreyanfranco@gmail.com
+                </a>{' '}
+                or through this form
+            </p>
+
+            <form className='mt-10 flex flex-col '>
+                <input className='h-14 px-4 rounded-lg borderBlack' type="email" placeholder='Your email' />
+                <textarea className='h-52 my-3 rounded-lg borderBlack p-4' placeholder='Your message' />
+                <button
+                    className='group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 '
+                    type='submit'
+                >
+                    Submit <FaPaperPlane className='text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1 ' />
+                </button>
+            </form>
+        </motion.section>
     )
 }
 
