33d263c4af58bec2865d66570dbe1c115d3b6a6e
dreyanfranco <dreyanfranco@gmail.com>
2023-09-29 13:50:17 +0200

    changed cv file

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -82,7 +82,7 @@ const Intro = () => {
                     Contact me here <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
                 </Link>
                 <a
-                    href="/Dreyan-Franco-CV.pdf"
+                    href="/Dreyan-Franco-CV-esp.pdf"
                     download
                     className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
                 >
