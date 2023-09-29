e0dc135960f4f6635703531cd6594c1abeb14aff
dreyanfranco <dreyanfranco@gmail.com>
2023-09-29 12:39:19 +0200

    changed date footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <footer className='mb-10 px-4 text-center text-gray-500'>
             <small className='mb-2 block text-xs'>
-                &copy; 2020 Dreyan. All rights reserved.
+                &copy; 2023 Dreyan. All rights reserved.
             </small>
             <p className='text-xs'>
                 <span className='font-semibold'>About this website:</span> built with React and Next.js (App Router & Server Actionss), TypeScript, Tailwind CSS, Framer Motion, React Email & Resend, Vercel hosting.
