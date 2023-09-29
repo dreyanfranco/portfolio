6112eac2ab53b0c43ebd8ae88ee47f80a89aebf1
dreyanfranco <dreyanfranco@gmail.com>
2023-09-29 13:31:30 +0200

    fixed icons of social network apps

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -88,20 +88,23 @@ const Intro = () => {
                 >
                     Download CV <HiDownload className="opacity-60 group-hover:translate-y-1" />
                 </a>
-                <a
-                    href="https://www.linkedin.com/in/dreyanfranco/"
-                    target="_blank"
-                    className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-                >
-                    <BsLinkedin />
-                </a>
-                <a
-                    href="https://github.com/dreyanfranco"
-                    target="_blank"
-                    className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-                >
-                    <FaGithubSquare />
-                </a>
+                <div className="flex gap-2">
+                    <a
+                        href="https://www.linkedin.com/in/dreyanfranco/"
+                        target="_blank"
+                        className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+                    >
+                        <BsLinkedin />
+                    </a>
+                    <a
+                        href="https://github.com/dreyanfranco"
+                        target="_blank"
+                        className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+                    >
+                        <FaGithubSquare />
+                    </a>
+
+                </div>
             </motion.div>
         </section>
     )
